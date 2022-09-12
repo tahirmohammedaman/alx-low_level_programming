7cf85f9db506d62a822965fcfad90bc1ab061ee2
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-12 16:05:26 +0300

    Complete task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
