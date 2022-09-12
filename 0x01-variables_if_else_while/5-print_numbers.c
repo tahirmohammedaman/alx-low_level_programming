e17c4e552f982e6ed675858e95f573bf6e8a4d79
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-12 15:51:38 +0300

    Complete task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
