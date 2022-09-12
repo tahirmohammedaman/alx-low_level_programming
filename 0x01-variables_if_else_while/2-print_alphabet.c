1fc034be95f41b2c58dafaeef953c3a799985a83
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-12 15:25:45 +0300

    Complete task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		putchar(ch++);
+	putchar('\n');
+
+	return (0);
+}
