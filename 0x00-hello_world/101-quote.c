fb87df6c6aaa64cdb3f0399754dd126346095f6a
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-12 14:40:34 +0300

    :happy_anime_noises:

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	fwrite("and that piece of art is useful\" ", 33, 1, stderr);
+	fwrite("- Dora Korpar, 2015-10-19\n", 26, 1, stderr);
 	exit(EXIT_FAILURE);
 }
