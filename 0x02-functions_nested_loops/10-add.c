fdfa8fc8da6ccbf63e362499d8a1323ca5a73d04
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-13 21:25:08 +0300

    Complete task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - return sum of two numbers
+ * @num1: input one
+ * @num2: input two
+ *
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
