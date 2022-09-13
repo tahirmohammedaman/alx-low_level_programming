f262f7ae7b949e0f9b13088a62749d03fc49a35b
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-13 20:47:47 +0300

    Complete task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints 0x9 multiplication table
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
+		{
+			if (i * j <= 9)
+			{
+				_putchar(' ');
+				_putchar((i * j) + '0');
+			}
+			else
+			{
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+		}
+}
