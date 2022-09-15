0de3b06238c5038f689ff1b3588583d92fa4b1be
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-15 17:55:29 +0300

    Complete task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if character is digit
+ * @c: input
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
