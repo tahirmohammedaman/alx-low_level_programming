3f8344851fd0d5a91b1914a43f6322c17f2fb3d4
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-15 22:57:54 +0300

    My 13th reason

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
 /**
- * print_diagonal - diagonal yimechegnal
+ * print_diagonal - **** diagonals
  * @n: input
  */
 void print_diagonal(int n)
 {
-	if (n > 0)
-	{
-		int i;
+	int i, j;
 
-		for (i = 0; i < n - 1; i++)
-			_putchar('_');
-		_putchar('\\');
-	}
-	_putchar('\n');
+	if (n < 1)
+		_putchar('\n');
+
+	for (i = 0; i <= n; i++)
+		for (j = 0; j < i; j++)
+		{
+			if (j == i - 1)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
 }
