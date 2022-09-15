879515fc81b87acf8a2605c1b2bb409e83111e78
Tahir Mohammedaman <tahirmohammedaman@gmail.com>
2022-09-16 02:10:57 +0300

    Do not look at code

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,57 @@
+#include "main.h"
+/**
+ * print_number - prints any number using _putchar
+ * @n: input number
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 0)
+	{
+		int nDigit = 0;
+		int tmp = n;
+
+		while (tmp != 0)
+		{
+			tmp /= 10;
+			nDigit++;
+		}
+
+		while (nDigit > 0)
+		{
+			if (n % 10 == 0)
+				_putchar('0');
+			else if (nDigit == 1)
+				_putchar((n % 10) + '0');
+			else
+				_putchar((n / _pow(10, nDigit - 1)) + '0');
+			n %= _pow(10, nDigit - 1);
+			nDigit--;
+		}
+	}
+}
+
+/**
+ * _pow - effin writing my own pow function
+ * @b: base
+ * @e: exponent
+ *
+ * Return: result
+ */
+int _pow(int b, int e)
+{
+	int pow = 1, i;
+
+	for (i = 0; i < e; i++)
+		pow *= b;
+
+	return (pow);
+}
